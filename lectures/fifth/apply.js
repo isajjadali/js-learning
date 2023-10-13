8f8c22396949dafeae7c277d8efea350988d1b48
ferasatali <ferasatali14@gmail.com>
2023-10-13 03:25:52 -0700

    TEST

--- lectures/fifth/apply.js
+++ lectures/fifth/apply.js
@@ -1,10 +1,31 @@
+function greet(message) {
+    console.log(message + ' ' + this.name);
+  }
+  
+  const person = {
+    name: 'Learning With US'
+  };
+  
+
+  greet.apply(person, ['Learn With US']); 
+  
+  
+console.log('Apply Method', 'Learning With Us');
+
+
+
+
+
+
+
+
 function getFullName(age, subject) {
     return this.fName + ' ' + this.lName + '=> ' + age + '=> ' + subject;
 }
 
 let user = {
-    fName: 'Sajjad',
-    lName: 'Ali',
+    fName: 'Learning',
+    lName: ' With US',
 };
 
 console.log(getFullName.apply(user, [21, 'Javascript']));
